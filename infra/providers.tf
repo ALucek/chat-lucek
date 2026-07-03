592d0be02036a745cd7cf6c5452d5dc69b6baa45
ALucek <adamrl2000@gmail.com>
2026-07-03 19:41:47 -0400

    fix(infra): set user_project_override for the billing budgets API

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,4 +1,8 @@
 provider "google" {
   project = var.project_id
   region  = var.region
+
+  # Billing budgets and some APIs require a quota project on the request.
+  user_project_override = true
+  billing_project       = var.project_id
 }
